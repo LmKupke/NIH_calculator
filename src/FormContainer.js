7f5bb58b652e0193e6ad156ff4b477ed3183b59c
Lincoln Kupke <lincolnkupke@gmail.com>
2016-12-05 14:44:28 -0500

    latest changes

---
+++ src/FormContainer.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+
+class FormContainer extends Component {
+  constructor(props) {
+   super(props);
+   this.state = {babyWeightGs: '', babyWeightKgs: ''};
+   this.convertToKG = this.convertToKG.bind(this);
+   this.convertToG = this.convertToG.bind(this);
+  }
+
+  convertToKG(event) {
+    this.setState({
+      babyWeightKgs: event.target.value/1000,
+      babyWeightGs: event.target.value
+    });
+  }
+  convertToG(event) {
+    this.setState({
+      babyWeightKgs: event.target.value,
+      babyWeightGs: event.target.value * 1000
+    });
+  }
+
+  render() {
+    return (
+
+    <div>
+      <label>
+          Weight (grams):
+          <input type="text" value={this.state.babyWeightGs} onChange={this.convertToKG} />
+          Weight (kg):
+              <input type="text" value={this.state.babyWeightKgs} onChange={this.convertToG} />
+      </label>
+    </div>
+    );
+  }
+}
+
+export default FormContainer;
