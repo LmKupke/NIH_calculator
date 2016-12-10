4e77160d1cd03c957a81d2aa7d34c8e8ebfa6693
Lincoln Kupke <lincolnkupke@gmail.com>
2016-12-10 12:40:15 -0500

    fixed calculation and added three labels

--- src/App.js
+++ src/App.js
@@ -13,7 +13,6 @@ class App extends Component {
         </div>
         <BabyWeight />
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
     );
--- src/TotalFluid.js
+++ src/TotalFluid.js
@@ -5,19 +5,22 @@ class TotalFluid extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      totalIntravenousFR: '',
+      totalIntravenousFR: '0',
+      totalIntravenousFRKG: '0',
       nonHalFR: '',
       halFR: '',
       intralipidFR: ''
     };
     this.totalIntFluidRate = this.totalIntFluidRate.bind(this);
     this.nonHalFRFunc = this.nonHalFRFunc.bind(this);
+    this.totalIntFluidRatemLKgHr = this.totalIntFluidRatemLKgHr.bind(this);
     // this.totalIntravenousFR = this.totalIntravenousFR.bind(this);
   }
 
   totalIntFluidRate(event) {
     this.setState({
-      totalIntravenousFR: event.target.value
+      totalIntravenousFR: event.target.value,
+      totalIntravenousFRKG: parseInt(event.target.value * 24)/ this.props.babyWeightKgs
     });
   }
   nonHalFRFunc(event) {
@@ -25,19 +28,27 @@ class TotalFluid extends Component {
       nonHalFR: event.target.value
     });
   }
+  totalIntFluidRatemLKgHr(event) {
+    this.setState({
+      totalIntravenousFR: parseInt(event.target.value * this.props.babyWeightKgs)/24,
+      totalIntravenousFRKG: event.target.value
+    });
+  }
 
   render() {
     return(
       <div>
         <div>
-          <label>Total Intravenous Fluid Rate:</label>
+          <label>Total Intravenous Fluid Rate(mL/kg/hr)</label>
+          <input type="text" onChange={this.totalIntFluidRatemLKgHr} value={this.state.totalIntravenousFRKG} />
+          <label>Total Intravenous Fluid Rate (mL/hr):</label>
           <input type="text" value={this.state.totalIntravenousFR} onChange={this.totalIntFluidRate} />
-          <input type="text" value={parseInt(this.state.totalIntravenousFR) * 24 * this.props.babyWeightKgs} />
-
         </div>
         <div>
-          <label>Non-HAL Fluid Rate:</label>
-          <input type="text" value={this.state.nonHalFR} onChange={this.nonHalFRFunc} />
+          <div><label>Non-HAL IV FLUIDS</label></div>
+        <div><label>Intralipid (20%)</label></div>
+      <div><b><label>Hyperalimentation</label></b></div>
+      <a href="mailto:lincolnkupke@gmail.com?Subject=Hello%20again" target="_top">Email Lincoln Kupke</a>
         </div>
 
       </div>
